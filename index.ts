78c76a86e09b6c2919546f44670364cd27110e8b
marlonfolken <marlonvargasmx@gmail.com>
2024-06-14 16:47:32 -0600

    Manejo de Router. Pasar peticiones a un archivo de users configurandole Router

--- index.ts
+++ index.ts
@@ -1,23 +1,10 @@
 //Ejemplo brevisimo de implementación de express con typescript.
 
 import express from 'express'
-
-type Usuario = {
-  id: string,
-  name: string,
-}
+import usersRoute from './users'
 
 const app = express()
 app.use(express.json()) // interpreta peticiones que llegan a través de POST.
-app.get('/', (req, res) => {
-  res.send('hola mundo')
-})
-
-app.post('/', (req, res) => {
-  const {name} = req.body as Usuario
-  console.log(name);
-  
-  res.send('ok');
-})
+app.use('/', usersRoute)
 
 app.listen(3000, () => console.log('Escuchando'))
